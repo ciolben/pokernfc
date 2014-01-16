af180bfa3f95e72103ab25672c1f6fa758c7ddb4
ciolben <loic.barben@epfl.ch>
2014-01-16 23:56:39 +0100

    Menus removed.

--- gen/ch/epfl/pokernfc/R.java
+++ gen/ch/epfl/pokernfc/R.java
@@ -114,11 +114,12 @@ public final class R {
     public static final class layout {
         public static final int activity_beam=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_player=0x7f030002;
-        public static final int activity_pot=0x7f030003;
-        public static final int activity_to_service=0x7f030004;
-        public static final int connection_dialog=0x7f030005;
-        public static final int number_picker_dialog_layout=0x7f030006;
+        public static final int activity_more=0x7f030002;
+        public static final int activity_player=0x7f030003;
+        public static final int activity_pot=0x7f030004;
+        public static final int activity_to_service=0x7f030005;
+        public static final int connection_dialog=0x7f030006;
+        public static final int number_picker_dialog_layout=0x7f030007;
     }
     public static final class menu {
         public static final int activity_to=0x7f080000;
