d946c46373caeda62e5e82ccc74d8677d20489b4
erico <vanini>
2014-01-16 15:21:39 +0100

    no nfc

--- gen/ch/epfl/pokernfc/R.java
+++ gen/ch/epfl/pokernfc/R.java
@@ -81,22 +81,28 @@ public final class R {
     public static final class id {
         public static final int GridLayout1=0x7f090002;
         public static final int LinearLayout1=0x7f090001;
-        public static final int action_settings=0x7f090017;
+        public static final int action_settings=0x7f09001d;
         public static final int beam_edit_text=0x7f090000;
         public static final int btPayCash=0x7f090009;
         public static final int button1=0x7f090015;
         public static final int buttonCall=0x7f09000a;
         public static final int buttonFold=0x7f09000c;
         public static final int buttonRaise=0x7f09000b;
-        public static final int nbPicker=0x7f090016;
+        public static final int nbPicker=0x7f09001c;
         public static final int playerCard1=0x7f090007;
         public static final int playerCard2=0x7f090008;
         public static final int player_card_layout=0x7f090006;
+        public static final int player_id=0x7f09001b;
         public static final int potCard1=0x7f090010;
         public static final int potCard2=0x7f090011;
         public static final int potCard3=0x7f090012;
         public static final int potCard4=0x7f090013;
         public static final int potCard5=0x7f090014;
+        public static final int pot_ip=0x7f090017;
+        public static final int pot_port=0x7f090019;
+        public static final int textView1=0x7f090016;
+        public static final int textView2=0x7f090018;
+        public static final int textView3=0x7f09001a;
         public static final int tvCash=0x7f090003;
         public static final int tvCashValue=0x7f090004;
         public static final int tvCurrentPot=0x7f09000d;
@@ -110,7 +116,8 @@ public final class R {
         public static final int activity_player=0x7f030002;
         public static final int activity_pot=0x7f030003;
         public static final int activity_to_service=0x7f030004;
-        public static final int number_picker_dialog_layout=0x7f030005;
+        public static final int connection_dialog=0x7f030005;
+        public static final int number_picker_dialog_layout=0x7f030006;
     }
     public static final class menu {
         public static final int activity_to=0x7f080000;
